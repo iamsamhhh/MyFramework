4d7b6a302471e998ae629e87c2a6b308d7a11cee
iamsam <81800116+iamsamhhh@users.noreply.github.com>
2024-11-17 16:57:53 +0800

    Update ScriptableObjectManagerWindow.cs
    Made category base on type

--- Assets/MyFramework/Framework/Utils/Editor/ScriptableObjectManagerWindow.cs
+++ Assets/MyFramework/Framework/Utils/Editor/ScriptableObjectManagerWindow.cs
@@ -1,71 +1,101 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using System;
 
 namespace MyFramework{
-public class ScriptableObjectEditor : EditorWindow
-{
-    private static ScriptableObjectEditor window;
-    private List<ScriptableObject> scriptableObjectList = new List<ScriptableObject>();
-    private ScriptableObject currenScriptableObject;
+    public class ScriptableObjectEditor : EditorWindow
+    {
+        private static ScriptableObjectEditor window;
+        private Dictionary<Type, List<ScriptableObject>> scriptableObjectDict = new Dictionary<Type, List<ScriptableObject>>();
+        private ScriptableObject currenScriptableObject;
+        private Vector2 scrollPosition;
+        private bool showUnityScriptableObject = false;
+        private const float LIST_WIDTH_PERCENTAGE = 0.3f;
 
         [MenuItem("MyFramework/Framework/Util/ScrptableObject Editor")]
-    public static void MenuClicked(){
-        window = GetWindow<ScriptableObjectEditor>();
-        window.titleContent = new GUIContent("ScriptableObject Editor");
-    }
-
-    private void OnGUI(){
-        DrawList();
-        DrawInspector();
-    }
-
-    private void OnEnable() {
-        RefreshList();
-    }
-
-    private void RefreshList(){
-        scriptableObjectList.Clear();
-        var assetGUIDs = AssetDatabase.FindAssets ("t: ScriptableObject");
-        foreach (var guid in assetGUIDs){
-            var path = AssetDatabase.GUIDToAssetPath(guid);
-            var scriptableObject = AssetDatabase.LoadAssetAtPath<ScriptableObject>(path);
-            if (scriptableObject)
-                scriptableObjectList.Add(scriptableObject);
+        public static void MenuClicked(){
+            window = GetWindow<ScriptableObjectEditor>();
+            window.titleContent = new GUIContent("ScriptableObject Editor");
         }
-    }
 
-    private void DrawList(){
-        GUILayout.BeginArea(
-            new Rect(0, 0, window.position.width * 0.25f, window.position.height), 
-            new GUIStyle("FrameBox")
-        );
+        private void OnGUI(){
+            DrawList();
+            DrawInspector();
+        }
 
-        foreach (var scriptableObject in scriptableObjectList){
-            if(!scriptableObject)
-                continue;
-            if (GUILayout.Button(scriptableObject.name)){
-                currenScriptableObject = scriptableObject;
+        private void OnEnable() {
+            RefreshList();
+        }
+
+        private void RefreshList(){
+            scriptableObjectDict.Clear();
+            var assetGUIDs = AssetDatabase.FindAssets("t: ScriptableObject");
+            foreach (var guid in assetGUIDs){
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var scriptableObject = AssetDatabase.LoadAssetAtPath<ScriptableObject>(path);
+                if (scriptableObjectDict.ContainsKey(scriptableObject.GetType())){
+                    scriptableObjectDict[scriptableObject.GetType()].Add(scriptableObject);
+                }
+                else{
+                    scriptableObjectDict.Add(scriptableObject.GetType(), new List<ScriptableObject>());
+                    scriptableObjectDict[scriptableObject.GetType()].Add(scriptableObject);
+                }
             }
         }
 
-        GUILayout.EndArea();
+        private void DrawList(){
+            GUILayout.BeginArea(
+                new Rect(0, 0, window.position.width * LIST_WIDTH_PERCENTAGE, window.position.height), 
+                new GUIStyle("FrameBox")
+            );
+            showUnityScriptableObject = GUILayout.Toggle(showUnityScriptableObject, new GUIContent("Show Unity ScriptableObjects"));
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            foreach (var record in scriptableObjectDict){
+                if (showUnityScriptableObject){
+                    EditorGUILayout.LabelField(record.Key.ToString(), EditorStyles.boldLabel);
+                    foreach (var scriptableObject in record.Value){
+                        if(!scriptableObject)
+                            continue;
+                        if (GUILayout.Button(scriptableObject.name)){
+                            currenScriptableObject = scriptableObject;
+                        }
+                    }
+                }
+                else{
+                    if (record.Key.ToString().Substring(0, 5) == "Unity") continue;
+                    EditorGUILayout.LabelField(record.Key.ToString(), EditorStyles.boldLabel);
+                    foreach (var scriptableObject in record.Value){
+                        if(!scriptableObject)
+                            continue;
+                        if (GUILayout.Button(scriptableObject.name)){
+                            currenScriptableObject = scriptableObject;
+                        }
+                    }
+                }
+            }
+
+            EditorGUILayout.EndScrollView();
+
+            GUILayout.EndArea();
+        }
+
+        private void DrawInspector(){
+            GUILayout.BeginArea (
+                new Rect(window.position.width * LIST_WIDTH_PERCENTAGE, y: 0, window.position.width * (1 - LIST_WIDTH_PERCENTAGE), window.position.height), 
+                new GUIStyle("FrameBox")
+            );
+
+            if(!currenScriptableObject) goto exit;
+
+            var editor = Editor.CreateEditor(currenScriptableObject);
+            editor.OnInspectorGUI();
+
+
+            exit:
+            GUILayout.EndArea();
+        }
     }
-
-    private void DrawInspector(){
-        GUILayout.BeginArea (
-            new Rect(window.position.width * .25f, y: 0, window.position.width * .75f, window.position.height), 
-            new GUIStyle("FrameBox")
-        );
-
-        if(!currenScriptableObject) goto exit;
-
-        var editor = Editor.CreateEditor(currenScriptableObject);
-        editor.OnInspectorGUI();
-
-
-        exit:
-        GUILayout.EndArea();
-    }
-}
 }
